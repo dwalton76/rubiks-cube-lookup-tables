d990abf639194209b43d2a8a56c693fbb3419679
Daniel Walton <dwalton76@gmail.com>
2018-08-22 23:01:38 -0400

    Fix self.lt_centers_max_depth

--- buildercore.py
+++ buildercore.py
@@ -744,8 +744,13 @@ class BFS(object):
                     "5x5x5-edges-second-four",
                 ):
                 lt_centers_filename = "lookup-table-5x5x5-step30-ULFRBD-centers-solve-unstaged.txt.5-deep" # dwalton
-                #self.lt_centers_max_depth = 5
-                self.lt_centers_max_depth = 4
+
+                if lt_centers_filename.endswith("5-deep"):
+                    self.lt_centers_max_depth = 5
+                elif lt_centers_filename.endswith("4-deep"):
+                    self.lt_centers_max_depth = 4
+                else:
+                    raise Exception()
 
             elif self.name == "5x5x5-edges-stage-second-four":
                 lt_centers_filename = "lookup-table-5x5x5-step101-ULFRBD-centers-solve-unstaged.txt"
@@ -765,7 +770,9 @@ class BFS(object):
                     self.lt_centers[state] = len(steps.split())
             log.info("end loading %s" % lt_centers_filename)
 
-            if lt_centers_filename == "lookup-table-5x5x5-step30-ULFRBD-centers-solve-unstaged.txt.5-deep":
+            if (lt_centers_filename == "lookup-table-5x5x5-step30-ULFRBD-centers-solve-unstaged.txt.5-deep" or
+                lt_centers_filename == "lookup-table-5x5x5-step30-ULFRBD-centers-solve-unstaged.txt.4-deep"):
+
                 log.info("begin loading %s json" % lt_centers_filename)
                 with open(lt_centers_filename + ".json", "r") as fh:
                     self.lt_centers_json = json.load(fh)
