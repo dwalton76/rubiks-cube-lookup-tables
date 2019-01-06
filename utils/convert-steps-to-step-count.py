8457751850133ff92ba0dd8c79048b29811ac3a6
Daniel Walton <dwalton76@gmail.com>
2019-01-06 08:58:04 -0500

    Add utils files from NxNxN solver repo

--- utils/convert-steps-to-step-count.py
+++ utils/convert-steps-to-step-count.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 
 import sys
-import subprocess
 
 filename = sys.argv[1]
-filename_final = filename + '.final'
 
-with open(filename_final, 'w') as fh_final:
+with open(filename + '.final', 'w') as fh_final:
     with open(filename, 'r') as fh:
         for line in fh:
             (state, steps) = line.rstrip().split(':')
             fh_final.write("%s:%d\n" % (state, len(steps.split())))
-
-subprocess.check_output("./utils/pad-lines.py %s" % filename_final, shell=True)
