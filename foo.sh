d8985696adf69390b120a4ac6d509b3cc1b3ada3
Daniel Walton <dwalton76@gmail.com>
2019-01-24 08:50:08 -0500

    updated foo.sh

--- foo.sh
+++ foo.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-#./utils/expand-state-with-centers-solved.py --centers-filename centers_solutions_555.txt --lookup-table-filename lookup-table-5x5x5-step100-stage-first-six-edges.txt --start 0 --end 1000000 &
-#./utils/expand-state-with-centers-solved.py --centers-filename centers_solutions_555.txt --lookup-table-filename lookup-table-5x5x5-step100-stage-first-six-edges.txt --start 1000001 --end 2000000 &
-#./utils/expand-state-with-centers-solved.py --centers-filename centers_solutions_555.txt --lookup-table-filename lookup-table-5x5x5-step100-stage-first-six-edges.txt --start 2000001 --end 3000000 &
-#./utils/expand-state-with-centers-solved.py --centers-filename centers_solutions_555.txt --lookup-table-filename lookup-table-5x5x5-step100-stage-first-six-edges.txt --start 3000001 --end 4000000 &
-#./utils/expand-state-with-centers-solved.py --centers-filename centers_solutions_555.txt --lookup-table-filename lookup-table-5x5x5-step100-stage-first-six-edges.txt --start 4000001 --end 5000000 &
-#./utils/expand-state-with-centers-solved.py --centers-filename centers_solutions_555.txt --lookup-table-filename lookup-table-5x5x5-step100-stage-first-six-edges.txt --start 5000001 --end 6000000 &
+./utils/expand-state-with-centers-solved.py --centers-filename centers_solutions_555.txt --lookup-table-filename lookup-table-5x5x5-step100-stage-first-six-edges.txt --start 0 --end 50000 &
+./utils/expand-state-with-centers-solved.py --centers-filename centers_solutions_555.txt --lookup-table-filename lookup-table-5x5x5-step100-stage-first-six-edges.txt --start 50001 --end 100000 &
+./utils/expand-state-with-centers-solved.py --centers-filename centers_solutions_555.txt --lookup-table-filename lookup-table-5x5x5-step100-stage-first-six-edges.txt --start 100001 --end 150000 &
+./utils/expand-state-with-centers-solved.py --centers-filename centers_solutions_555.txt --lookup-table-filename lookup-table-5x5x5-step100-stage-first-six-edges.txt --start 150001 --end 200000 &
 
 #./utils/expand-state-with-centers-solved.py --centers-filename horse_shoe_centers_solutions_555.txt --lookup-table-filename lookup-table-5x5x5-step100-solve-first-six-edges.txt --start 0 --end 2300000 --specific-depth 17 &
 #./utils/expand-state-with-centers-solved.py --centers-filename horse_shoe_centers_solutions_555.txt --lookup-table-filename lookup-table-5x5x5-step100-solve-first-six-edges.txt --start 2300001 --end 4600000 --specific-depth 17 &
@@ -15,4 +13,4 @@
 #./utils/expand-state-with-centers-solved.py --centers-filename horse_shoe_centers_solutions_555.txt --lookup-table-filename lookup-table-5x5x5-step100-solve-first-six-edges.txt --start 11500001 --end 14000000 --specific-depth 17 &
 
 
-./utils/expand-state-with-centers-solved-no-pickle.py --centers-filename horse_shoe_centers_solutions_555.txt --lookup-table-filename lookup-table-5x5x5-step100-solve-first-six-edges.txt --specific-depth 17
+#./utils/expand-state-with-centers-solved-no-pickle.py --centers-filename horse_shoe_centers_solutions_555.txt --lookup-table-filename lookup-table-5x5x5-step100-solve-first-six-edges.txt --specific-depth 17
