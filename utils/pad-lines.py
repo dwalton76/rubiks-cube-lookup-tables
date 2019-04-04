b4351a4bcc30207e72ccbba6cfcc365cedb820aa
Daniel Walton <dwalton76@gmail.com>
2019-04-04 07:57:19 -0400

    pad_lines batch writes

--- utils/pad-lines.py
+++ utils/pad-lines.py
@@ -15,6 +15,9 @@ with open(filename, 'r') as fh:
 
 print("%s max_length: %d" % (filename, max_length))
 line_number = 0
+WRITE_BATCH_SIZE = 1000000
+to_write = []
+to_write_count = 0
 
 with open(filename_pad, 'w') as fh_pad:
     with open(filename, 'r') as fh:
@@ -25,10 +28,23 @@ with open(filename_pad, 'w') as fh_pad:
 
             if spaces_to_add:
                 line = line + ' ' * spaces_to_add
-            fh_pad.write(line + '\n')
+
+            to_write.append(line)
+            to_write_count += 1
+
+            if to_write_count >= WRITE_BATCH_SIZE:
+                fh_pad.write("\n".join(to_write) + "\n")
+                to_write = []
+                to_write_count = 0
+
             line_number += 1
 
             #if line_number % 100000 == 0:
             #    print(line_number)
+
+    if to_write_count:
+        fh_pad.write("\n".join(to_write) + "\n")
+        to_write = []
+        to_write_count = 0
 
 shutil.move(filename_pad, filename)
