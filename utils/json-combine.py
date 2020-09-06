a4bd21516473ff1df440c43ef3d2340a8c7b1811
Daniel Walton <dwalton76@gmail.com>
2020-09-06 09:34:56 -0400

    added utils/json-combine.py

---
+++ utils/json-combine.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# standard libraries
+import logging
+import os
+import shutil
+import sys
+from typing import List
+
+
+def json_combine_files(filenames: List[str]) -> None:
+    output_filename = "/tmp/json_combine_files.txt"
+
+    with open(output_filename, "w") as fh_final:
+        # start with a {
+        fh_final.write("{\n")
+
+        for (index, filename) in enumerate(filenames):
+
+            # If this is not the first file, write a "," to continue the json
+            # from the previous file
+            if index > 0:
+                fh_final.write(",\n")
+
+            with open(filename, "r") as fh:
+                lines = fh.readlines()
+
+                # chop the first and last line
+                lines = lines[1:-1]
+
+                # write everything else
+                fh_final.write("\n".join(lines))
+
+            # rm the file
+            os.unlink(filename)
+
+        # end with a }
+        fh_final.write("\n}\n")
+
+    # save all output to the last filename
+    shutil.move(output_filename, filenames[-1])
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(filename)20s %(levelname)8s: %(message)s")
+    log = logging.getLogger(__name__)
+
+    filenames = sys.argv[1:]
+    json_combine_files(filenames)
