a70023bc3fe89e19624f41d0c7c74fe64db1d956
Daniel Walton <dwalton76@gmail.com>
2021-09-09 11:07:30 -0400

    chop 555 phase12 experiment

--- utils/build-ida-graph.py
+++ utils/build-ida-graph.py
@@ -29,8 +29,6 @@ from rubikscubennnsolver.RubiksCube555 import (
     LookupTable555Phase6HighEdgeMidge,
     LookupTable555Phase6LowEdgeMidge,
     LookupTable555UDCenterSolve,
-    LookupTable555UDCenterStageTCenter,
-    LookupTable555UDCenterStageXCenter,
     RubiksCube555,
     solved_555,
 )
@@ -95,14 +93,6 @@ elif lt_class.startswith("Build555"):
 
     elif lt_class == "Build555LRCenterStageXCenter":
         cube.lt = LookupTable555LRXCenterStage(cube, build_state_index=True)
-        cube.lt.build_ida_graph()
-
-    elif lt_class == "Build555UDCenterStageTCenter":
-        cube.lt = LookupTable555UDCenterStageTCenter(cube, build_state_index=True)
-        cube.lt.build_ida_graph()
-
-    elif lt_class == "Build555UDCenterStageXCenter":
-        cube.lt = LookupTable555UDCenterStageXCenter(cube, build_state_index=True)
         cube.lt.build_ida_graph()
 
     # phase 2
